bac1a5c22681e0432f4fea4d034eee20ff750ae4
seansaito <saitosean@ymail.com>
2020-06-05 17:23:24 +0800

    Fixed typo

--- scripts/robustness_tabular.sh
+++ scripts/robustness_tabular.sh
@@ -2,5 +2,5 @@
 
 python -m experiments.robustness_tabular_experiment \
 --log_out=experiments/logs/robustness_tabular.log \
---save_dir=experiments/precision_tabular_results/
+--save_dir=experiments/robustness_tabular_results/
 
